e23aefa548ea182a6be57282fa8c4edc6804de62
micmarsh <michael.marsh42@gmail.com>
2014-01-20 17:16:55 -0500

    upgrade overnight version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [overnight "0.1.2"]
+                 [overnight "0.1.3"]
                  [prismatic/schema "0.2.0"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
